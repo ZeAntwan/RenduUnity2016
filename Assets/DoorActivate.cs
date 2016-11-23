57caaee9945bb4cf4f94cb225f3276eabac0080e
Antoine SARRAZIN <tony78.itouch@gmail.com>
2016-11-23 14:13:22 +0100

    Update

---
+++ Assets/DoorActivate.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorActivate : MonoBehaviour {
+    public GameObject door;
+	// Use this for initialization
+	void Start () {
+        door = GameObject.Find("SecretWall");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            Debug.Log("True");
+            //Emmettre un effet sonore ?
+
+            //Autoriser l'acces à la montagne
+            Destroy(door);
+            
+
+            //Lancer la musique ?
+        }
+    }
+}
